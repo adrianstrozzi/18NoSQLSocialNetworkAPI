3d63c7426076adaf159cc105f52fb48fafa2f83b
Adrian Strozzi <adrianstrozzi@gmail.com>
2022-07-15 16:49:32 -0500

    Finished Models 1st Ver

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const validateEmail = (email) => {
+  const re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  return re.test(email);
+};
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Please fill a valid email address"
+      ],
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'thoughts' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
