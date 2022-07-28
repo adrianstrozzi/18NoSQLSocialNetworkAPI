bc53958a2e42c7462db15d5a4f3e5791769bbfc0
Adrian Strozzi <adrianstrozzi@gmail.com>
2022-07-28 11:12:51 -0500

    Added Routes for user and thoughts

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  createFriend,
+  deleteFriend
+} = require('../../controllers/userController.js');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+
+router
+  .route('/:friendId')
+  .post(createFriend)
+  .delete(deleteFriend)
+
+module.exports = router;
